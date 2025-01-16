c05be33466970e1752fd4a3fea7789ebfcbdccb0
Yuxiang <128219376+Sethanyx@users.noreply.github.com>
2025-01-15 18:57:13 -0600

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,30 +1,27 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project information
 project = 'PyGIP'
-copyright = '2024, Yuxiang Sun1, Chenxi Zhao'
+copyright = '2024, Yuxiang Sun, Chenxi Zhao'
 author = 'Yuxiang Sun, Chenxi Zhao'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinx.ext.autodoc',        
-    'sphinx.ext.napoleon',      
-    'sphinx.ext.viewcode',       
-    'sphinx.ext.autosummary',    
-    'sphinx_autodoc_typehints'   
-    ]
+# General configuration
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.githubpages'  # Added for GitHub Pages support
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Autodoc configuration
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
@@ -32,16 +29,17 @@ autodoc_default_options = {
     'show-inheritance': True,
 }
 
+# Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML theme settings
 html_theme = 'furo'
 html_static_path = ['_static']
-
 html_theme_options = {
-    "navigation_with_keys": True, 
-    "sidebar_hide_name": False,   
+    "navigation_with_keys": True,
+    "sidebar_hide_name": False,
 }
+
+# Important for GitHub Pages
+html_baseurl = '/pygip.github.io/'
